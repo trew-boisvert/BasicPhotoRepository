7b8703b17fcded3705f8bf2baf807e51d9c681d3
Trew Boisvert <trewboisvert@gmail.com>
2021-04-27 05:04:46 +0000

    Create user and post classes in model.py

---
+++ model.py
@@ -0,0 +1,54 @@
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+from datetime import datetime, timezone, timedelta
+
+db = SQLAlchemy()
+
+class User(db.Model):
+    """Data model for a user."""
+
+    __tablename__ = "users"
+
+    user_id = db.Column(db.Integer, primary_key=True, autoincrement=True, nullable=False)
+    user_email = db.Column(db.String(100), nullable=False, unique=True)
+    user_password = db.Column(db.String(100), nullable=False)
+    user_name = db.Column(db.String(100), nullable=False)
+
+    post = db.relationship('Post')
+
+    def __repr__(self):
+        """Show info about user."""
+
+        return f"<User_id= {self.user_id} user_name= {self.user_name}>"
+
+class Post(db.Model):
+    """Data model for posts to photo board."""
+
+    __tablename__ = "posts"
+
+    post_id = db.Column(db.Integer, primary_key=True, autoincrement=True, nullable=False)
+    user_id = db.Column(db.Integer, db.ForeignKey('users.user_id'), nullable=False)
+    post_date = db.Column(db.DateTime, nullable=False)
+    post_title = db.Column(db.String(), nullable=False)
+    post_comment = db.Column(db.String(), nullable=True)
+    post_photo_link = db.Column(db.String(), nullable=False)
+
+    user = db.relationship('User')
+
+    def __repr__(self):
+        return f"<Post id= {self.post_id}>"
+
+def connect_to_db(app):
+    """Connect the database to our Flask app."""
+
+    app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql:///pics'
+    app.config['SQLALCHEMY_ECHO'] = False
+    app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+    db.app = app
+    db.init_app(app)        
+
+if __name__ == '__main__':
+    from server import app
+    connect_to_db(app)
+    db.create_all()
+    print('Connected to db!')
