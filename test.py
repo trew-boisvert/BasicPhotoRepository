a78daa196cdd47a588c63a3bd904b9f41254d75e
Trew Boisvert <trewboisvert@gmail.com>
2021-05-05 04:42:57 +0000

    Add exploratory testing criteria to test.py

---
+++ test.py
@@ -0,0 +1,26 @@
+""" Exploratory Testing
+List of Features:
+    Sign up for account
+        Check database for new entry
+        Check entry matches info added
+    Login to account
+        Check login works for existing account
+        Check login fails for non-existent account
+    View Profile
+        Check profile displayed matches account logged into
+    Upload Photo to board
+        Check database for new entry
+        Check discussion board to see if photo is displayed
+        Check user profile to see if post is displayed with a delete option
+    Delete photo posts from profile
+        Check that entry is removed from database
+        Check that photo is removed from discussion board
+        Check that post is removed from profile
+    Logout from account
+        Check that profile is unaccessible
+        Check that user still exists in database
+    Delete user account
+        Check that user account is no longer in database
+        Check that pages only viewable by a user are unaccessible
+
+"""
