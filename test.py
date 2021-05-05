8b033c46ba4efe4b5c786e78429d6cbd88d525f7
Trew Boisvert <trewboisvert@gmail.com>
2021-05-05 05:46:07 +0000

    Add exploratory testing criteria to test.py

--- test.py
+++ test.py
@@ -1,25 +1,47 @@
 """ Exploratory Testing
 List of Features:
+    Navbar
+        Test that buttons redirect to appropriate pages
     Sign up for account
+        Input type:
+            Username - text box
+            Password - password box
+            Email - email box
+            Button - click
         Check database for new entry
         Check entry matches info added
     Login to account
+        Input type:
+            Email - email box
+            Password - password box
+            Button - click
         Check login works for existing account
         Check login fails for non-existent account
     View Profile
         Check profile displayed matches account logged into
     Upload Photo to board
+        Input type:
+            Name of post - text box
+            Comment on post - text box
+            Choose File button - file selection on click
+            Upload - submit on click
         Check database for new entry
         Check discussion board to see if photo is displayed
         Check user profile to see if post is displayed with a delete option
     Delete photo posts from profile
+        Input type:
+            Button - click
         Check that entry is removed from database
         Check that photo is removed from discussion board
         Check that post is removed from profile
     Logout from account
+        Input type:
+            Button - click
         Check that profile is unaccessible
         Check that user still exists in database
     Delete user account
+        Input type:
+            Button - click
         Check that user account is no longer in database
         Check that pages only viewable by a user are unaccessible
 
